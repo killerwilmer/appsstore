46de286eec8b0eca0d4e4192bff268b924458fc5
Wilmer <killerwilmer@gmail.com>
2017-01-11 17:03:20 -0500

    first commit

---
+++ app/src/main/java/com/grability/appsstore/util/Constantes.java
@@ -0,0 +1,9 @@
+package com.grability.appsstore.util;
+
+/**
+ * Created by Killerwilmer
+ */
+public class Constantes {
+
+    public static String BASE_URL = "https://itunes.apple.com/us/rss/topfreeapplications/limit=2/json"; //BASE URL
+}
